b56b634680e22895354f3c7d9a4849961f628ca3
d11craigp <46790937+trikraig@users.noreply.github.com>
2024-03-10 19:35:57 +0000

    New Character Base and Player Enemy Classes

---
+++ Source/LunaEpoc/Character/LunaEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LunaCharacterBase.h"
+#include "LunaEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAEPOC_API ALunaEnemy : public ALunaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LunaEpoc/Character/PlayerLunaCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LunaCharacterBase.h"
+#include "PlayerLunaCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAEPOC_API APlayerLunaCharacter : public ALunaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
