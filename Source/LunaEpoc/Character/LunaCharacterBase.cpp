83d3b5da60ded16b125446ce220dcea3455fc55b
d11craigp <46790937+trikraig@users.noreply.github.com>
2024-03-12 09:29:15 +0000

    New Character BP Setup
    Not to use yet, trialling as part of training course, just use original for now

--- Source/LunaEpoc/Character/LunaCharacterBase.cpp
+++ Source/LunaEpoc/Character/LunaCharacterBase.cpp
@@ -7,6 +7,10 @@ ALunaCharacterBase::ALunaCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 void ALunaCharacterBase::BeginPlay()
